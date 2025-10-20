8a6a80601d503c08697266e69cc59edb256bde0d
William Banham <will_banh@icloud.com>
2025-10-20 16:16:28 +0100

    Beginnings of leveling dashboard page

--- bot/src/api/modules/leveling.ts
+++ bot/src/api/modules/leveling.ts
@@ -8,64 +8,117 @@ import {
 import { db } from "@/db";
 import {
   guildLevel,
+  guildLevelMultiplier,
   GuildLevelSelect,
   leveling,
   LevelingSelect,
 } from "@/db/schema";
 import { and, eq } from "drizzle-orm";
 import { app } from "@/index";
+import { getGuildConfig } from "@/utils";
+import { Role } from "discord.js";
 
 export const levelingModule = new Elysia({
   prefix: "/leveling",
-}).get(`/:id/leaderboard`, async (context) => {
-  try {
-    const authResult = await createAuthMiddleware()(context);
-    if (authResult) return authResult;
+})
+  .get(`/:id`, async (context) => {
+    try {
+      const authResult = await createAuthMiddleware()(context);
+      if (authResult) return authResult;
 
-    const { auth } = context as unknown as { auth: AuthenticatedContext };
+      const { auth } = context as unknown as { auth: AuthenticatedContext };
+      const data = {};
 
-    const levelingData = await db
-      .select()
-      .from(leveling)
-      .where(and(eq(leveling.guildId, auth.guild.id)));
+      const guildConfig = await getGuildConfig(auth.guild.id);
+      if (!guildConfig)
+        return createErrorResponse("Failed to find guild configuration.");
 
-    let data: (
-      | (LevelingSelect & { level: GuildLevelSelect })
-      | LevelingSelect
-    )[] = [];
+      const levels = await db
+        .select()
+        .from(guildLevel)
+        .where(eq(guildLevel.guildId, auth.guild.id));
 
-    for (let i = 0; i < levelingData.length; i++) {
-      const level = levelingData[i] as LevelingSelect;
-      const newLevel = {
-        ...level,
-        user: auth.guild.members.cache.find((f) => f.id === level.userId),
-      };
+      if (!levels || !levels[0])
+        return createErrorResponse("Failed to find levels data.");
 
-      if (newLevel.levelId) {
-        const levelData = await db
-          .select()
-          .from(guildLevel)
-          .where(eq(guildLevel.id, newLevel.levelId));
-        if (levelData && levelData[0]) {
-          data.push({
-            ...newLevel,
-            level: levelData[0],
-          });
+      const multipliers = await db
+        .select()
+        .from(guildLevelMultiplier)
+        .where(eq(guildLevelMultiplier.guildId, auth.guild.id));
+
+    
+
+      const levelsWithRoles: (GuildLevelSelect & { role: Role | null})[] = [];
+      for(let i = 0; i < levels.length; i++) {
+        const level = levels[i]
+        const role = auth.guild.roles.cache.find(f => f.id === level.roleId);
+        levelsWithRoles.push({
+          ...level,
+          role: role || null
+        })
+      }
+
+      return createSuccessResponse({
+        ok: true,
+        data: {
+          toggled: guildConfig.leveling,
+          levels: levelsWithRoles,
+          multipliers: multipliers && multipliers[0] ? multipliers : []
+        },
+      });
+    } catch (e) {
+      return createErrorResponse("Unable to find leveling data.");
+    }
+  })
+  .get(`/:id/leaderboard`, async (context) => {
+    try {
+      const authResult = await createAuthMiddleware()(context);
+      if (authResult) return authResult;
+
+      const { auth } = context as unknown as { auth: AuthenticatedContext };
+
+      const levelingData = await db
+        .select()
+        .from(leveling)
+        .where(and(eq(leveling.guildId, auth.guild.id)));
+
+      let data: (
+        | (LevelingSelect & { level: GuildLevelSelect })
+        | LevelingSelect
+      )[] = [];
+
+      for (let i = 0; i < levelingData.length; i++) {
+        const level = levelingData[i] as LevelingSelect;
+        const newLevel = {
+          ...level,
+          user: auth.guild.members.cache.find((f) => f.id === level.userId),
+        };
+
+        if (newLevel.levelId) {
+          const levelData = await db
+            .select()
+            .from(guildLevel)
+            .where(eq(guildLevel.id, newLevel.levelId));
+          if (levelData && levelData[0]) {
+            data.push({
+              ...newLevel,
+              level: levelData[0],
+            });
+          } else {
+            data.push(newLevel);
+          }
         } else {
           data.push(newLevel);
         }
-      } else {
-        data.push(newLevel);
       }
-    }
 
-    return createSuccessResponse({
-      ok: true,
-      data: data.sort((a, b) => b.xp - a.xp),
-    });
-  } catch (e) {
-    return createErrorResponse(
-      "Unable to find leveling leaderboard statisitcs."
-    );
-  }
-});
+      return createSuccessResponse({
+        ok: true,
+        data: data.sort((a, b) => b.xp - a.xp),
+      });
+    } catch (e) {
+      return createErrorResponse(
+        "Unable to find leveling leaderboard statisitcs."
+      );
+    }
+  });
--- src/app/dashboard/[id]/leveling/page.tsx
+++ src/app/dashboard/[id]/leveling/page.tsx
@@ -1,17 +1,30 @@
 "use client";
 
+import DefaultInput from "@/components/Input";
 import Loader from "@/components/loader";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
 import { env } from "@/env";
 import { User } from "@/lib/auth/client";
-import { GuildLevelSelect, LevelingSelect } from "@/lib/db/difference";
+import {
+  GuildLevelMultiSelect,
+  GuildLevelSelect,
+  LevelingSelect,
+} from "@/lib/db/difference";
 import { useGuildStore, useUserStore } from "@/lib/stores";
 import { tw } from "@/lib/utils";
 import { betterFetch } from "@better-fetch/fetch";
 import { useMutation } from "@tanstack/react-query";
 import { set } from "date-fns";
-import { Guild, GuildMember } from "discord.js";
-import { Badge } from "lucide-react";
+import { Guild, GuildMember, Role } from "discord.js";
+import { Badge, PlusIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
@@ -26,14 +39,128 @@ export default function Leveling() {
 
   return (
     <div className="flex flex-row gap-3">
-      <LevelingConfig />
+      <LevelingConfig guild={guild} user={user} />
       <LevelingLeaderboard guild={guild} user={user} />
     </div>
   );
 }
 
-export function LevelingConfig() {
-  return <Card className="flex w-[70%]"></Card>;
+export function LevelingConfig({ user, guild }: PassedDownProps) {
+  const [toggle, setToggle] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const [data, setData] = useState<{
+    levels: (GuildLevelSelect & { role: Role | null })[];
+    multipliers: GuildLevelMultiSelect[];
+  }>();
+
+  const { mutateAsync: fetchLevelingAsync } = useMutation({
+    mutationKey: ["fetchLevelData", "dashboard"],
+    mutationFn: async () =>
+      betterFetch<{
+        data: {
+          ok: boolean;
+          data: {
+            toggled: boolean;
+            levels: (GuildLevelSelect & { role: Role | null })[];
+            multipliers: GuildLevelMultiSelect[];
+          };
+        };
+      }>(`${env.NEXT_PUBLIC_API_URL}/modules/leveling/${guild?.id}/`, {
+        method: "GET",
+        headers: {
+          "bearer-user-id": `${user?.user_id}`,
+          "bearer-authorization": `${user?.authentication_token}`,
+        },
+
+        onRequest: () => {
+          console.log(
+            `Sending request to /modules/embeds-and-messages/${guild?.id}/`
+          );
+        },
+        onError: (error: any) => {
+          console.error("API request failed:", error);
+          toast.error(
+            "Failed to fetch leveling data, please refresh to try again."
+          );
+        },
+      }),
+  });
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchLevelingAsync();
+      console.log(data.data?.data.data);
+      if (data && data.data) {
+        setLoading(false);
+        setToggle(data.data.data.data.toggled);
+        setData({
+          levels: data.data.data.data.levels,
+          multipliers: data.data.data.data.multipliers,
+        });
+      }
+    })();
+  }, []);
+
+  return (
+    <Card className="flex w-[70%]">
+      <CardHeader>
+        <CardTitle>Leveling Configuration</CardTitle>
+        <CardDescription className="max-w-[500px]">
+          Leveling keeps your community active and engaged. Members earn XP by
+          chatting, climbing ranks, and unlocking roles—it’s a fun way to reward
+          participation and build interaction.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <DefaultInput
+          className="flex flex-row gap-4 items-center"
+          title="Toggle"
+        >
+          <Switch
+            checked={toggle}
+            onCheckedChange={() => setToggle(!toggle)}
+            className="cursor-pointer"
+          />
+        </DefaultInput>
+
+        <div className="flex my-[2rem] flex-row justify-between items-center">
+          <h1 className="leading-none font-semibold">Levels</h1>
+          <Button variant={"secondary"} className="cursor-pointer">
+            Add Level
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          {data?.levels.map((level, i) => {
+            return (
+              <div key={i} className="flex flex-row justify-between">
+                <div className="flex flex-row  items-center gap-3">
+                  <div className="bg-input/30 text-xs  px-2 py-1 rounded-md border border-blimp-border">
+                    {level.level}
+                  </div>
+                  <RoleDisplay role={level.role} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export function RoleDisplay({ role }: { role: Role | null }) {
+  return (
+    <div
+      style={{
+        border: `1px solid ${role?.hexColor}`,
+      }}
+      className="rounded-lg px-2"
+    >
+      {role?.name}
+    </div>
+  );
 }
 
 type MixedLevelData = LevelingSelect & { user: GuildMember };
@@ -102,8 +229,11 @@ export function LevelingLeaderboard({
             {" "}
             {levelingData.map((d, i) => {
               return (
-                <div key={d.id} className="flex flex-row justify-between py-[1.5rem] px-[0.5rem]">
-                  {/* <LevelPosition spot={i + 1} /> */}
+                <div
+                  key={d.id}
+                  className="flex flex-row justify-between py-[1.5rem] px-[0.5rem]"
+                >
+                  <LevelPosition spot={i + 1} />
                 </div>
               );
             })}
@@ -129,7 +259,9 @@ function LevelPosition({ spot }: { spot: number }) {
     >
       <div className="flex w-fit items-center justify-center">
         <Badge />
-        <div className="flex text-xs w-full h-full z-[10] absolute justify-center items-center">{spot}</div>
+        <div className="flex text-xs w-full h-full z-[10] absolute justify-center items-center">
+          {spot}
+        </div>
       </div>
     </div>
   );
