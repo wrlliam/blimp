ec4ce477e295a14b1ddb82a51c42653c8a1a763f
William Banham <will_banh@icloud.com>
2025-10-27 14:26:21 +0000

    changes to redis connection

--- bot/src/db/redis.ts
+++ bot/src/db/redis.ts
@@ -1,6 +1,4 @@
 import { env } from "@/env";
 import { RedisClient } from "bun";
 
-export const redis = new RedisClient(env.REDIS_URL, {
-  autoReconnect: true,
-});
+export const redis = new RedisClient(env.REDIS_URL);
