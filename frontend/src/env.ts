4bae7c0916a6b5593c3d00cda7190b4f3b6e8c71
William Banham <will_banh@icloud.com>
2025-10-27 11:01:48 +0000

    test 1

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -3,24 +3,17 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    // NODE_ENV: z.enum(["development", "test", "production"]),
     BETTER_AUTH_SECRET: z.string(),
     BETTER_AUTH_URL: z.string().url(),
     DATABASE_URL: z.string(),
-    // REDIS_URL: z.string().optional(),
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
-    // TODO: UNDO AFTER BRING BACK IMPLEMENTATION
-    // STRIPE_PRIVATE_KEY: z.string().optional(),
-    // STRIPE_WEBHOOK_SECRET: z.string().optional(),
     API_PORT: z.string(),
     GUILD_ID: z.string().optional(),
     WS_PORT: z.string(),
   },
   client: {
     NEXT_PUBLIC_URL: z.string().url(),
-    // TODO: UNDO AFTER BRING BACK IMPLEMENTATION
-    // NEXT_PUBLIC_STRIPE_PUBLIC_KEY: z.string().optional(),
     NEXT_PUBLIC_API_URL: z.string(),
     NEXT_PUBLIC_WS_URL: z.string(),
     NEXT_PUBLIC_DISCORD_BOT_INVITE_URL: z.string(),
@@ -30,12 +23,8 @@ export const env = createEnv({
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
-    // REDIS_URL: process.env.REDIS_URL,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    // NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
-    // STRIPE_PRIVATE_KEY: process.env.STRIPE_PRIVATE_KEY,
-    // STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
     API_PORT: process.env.API_PORT,
     GUILD_ID: process.env.GUILD_ID,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
